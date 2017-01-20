239dac5a41e4cd7a1d8237c6abfc40236a935c10
Media Streaming Websites <support@s3bubble.com>
2017-01-20 16:51:22 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# videojs-s3bubble-meta-overlay
+
+A simple overlay for S3Bubble video meta data title and description
+
+## Table of Contents
+
+<!-- START doctoc -->
+<!-- END doctoc -->
+## Installation
+
+```sh
+npm install --save videojs-s3bubble-meta-overlay
+```
+
+The npm installation is preferred, but Bower works, too.
+
+```sh
+bower install  --save videojs-s3bubble-meta-overlay
+```
+
+## Usage
+
+To include videojs-s3bubble-meta-overlay on your website or web application, use any of the following methods.
+
+### `<script>` Tag
+
+This is the simplest case. Get the script in whatever way you prefer and include the plugin _after_ you include [video.js][videojs], so that the `videojs` global is available.
+
+```html
+<script src="//path/to/video.min.js"></script>
+<script src="//path/to/videojs-s3bubble-meta-overlay.min.js"></script>
+<script>
+  var player = videojs('my-video');
+
+  player.s3BubbleMetaOverlay();
+</script>
+```
+
+### Browserify
+
+When using with Browserify, install videojs-s3bubble-meta-overlay via npm and `require` the plugin as you would any other module.
+
+```js
+var videojs = require('video.js');
+
+// The actual plugin function is exported by this module, but it is also
+// attached to the `Player.prototype`; so, there is no need to assign it
+// to a variable.
+require('videojs-s3bubble-meta-overlay');
+
+var player = videojs('my-video');
+
+player.s3BubbleMetaOverlay();
+```
+
+### RequireJS/AMD
+
+When using with RequireJS (or another AMD library), get the script in whatever way you prefer and `require` the plugin as you normally would:
+
+```js
+require(['video.js', 'videojs-s3bubble-meta-overlay'], function(videojs) {
+  var player = videojs('my-video');
+
+  player.s3BubbleMetaOverlay();
+});
+```
+
+## License
+
+MIT. Copyright (c) Media Streaming Websites &lt;support@s3bubble.com&gt;
+
+
+[videojs]: http://videojs.com/
---
+++ src/plugin.js
@@ -0,0 +1,116 @@
+import videojs from 'video.js';
+
+// Default options for the plugin.
+const defaults = {};
+
+/**
+ * Function to invoke when the player is ready.
+ *
+ * This is a great place for your plugin to initialize itself. When this
+ * function is called, the player will have its DOM and child components
+ * in place.
+ *
+ * @function onPlayerReady
+ * @param    {Player} player
+ * @param    {Object} [options={}]
+ */
+const onPlayerReady = (player, options) => {
+
+    console.log('options', options);
+
+    console.log(player.el());
+
+    var s3BubbleMetaOverlayWrap = document.createElement('div');
+    s3BubbleMetaOverlayWrap.id = "s3bubble-meta-overlay";
+    s3BubbleMetaOverlayWrap.className = 's3bubble-meta-overlay';
+
+    var s3BubbleMetaOverlayInnerWrap = document.createElement('div');
+    s3BubbleMetaOverlayInnerWrap.id = "s3bubble-meta-overlay-container";
+    s3BubbleMetaOverlayInnerWrap.className = 's3bubble-meta-overlay-container';
+
+    s3BubbleMetaOverlayWrap.appendChild(s3BubbleMetaOverlayInnerWrap);
+
+    if (options.subTitle) {
+        var s3BubbleMetaOverlayWrapSubTitle = document.createElement('h5');
+        s3BubbleMetaOverlayWrapSubTitle.innerHTML = options.subTitle;
+        s3BubbleMetaOverlayInnerWrap.appendChild(s3BubbleMetaOverlayWrapSubTitle);
+    }
+
+    if (options.title) {
+        var s3BubbleMetaOverlayWrapTitle = document.createElement('h1');
+        s3BubbleMetaOverlayWrapTitle.innerHTML = options.title;
+        s3BubbleMetaOverlayInnerWrap.appendChild(s3BubbleMetaOverlayWrapTitle);
+    }
+
+    if (options.para) {
+        var s3BubbleMetaOverlayWrapPara = document.createElement('p');
+        s3BubbleMetaOverlayWrapPara.innerHTML = options.para;
+        s3BubbleMetaOverlayInnerWrap.appendChild(s3BubbleMetaOverlayWrapPara);
+    }
+
+    player.el().appendChild(s3BubbleMetaOverlayWrap);
+
+    s3BubbleMetaOverlayWrap.onclick = function() {
+
+        player.play();
+    };
+
+    player.on('play', function() {
+
+        addClass(s3BubbleMetaOverlayWrap, 's3bubble-meta-overlay-hidden');
+
+    });
+
+    player.on('pause', function() {
+
+        removeClass(s3BubbleMetaOverlayWrap, 's3bubble-meta-overlay-hidden');
+
+    });
+
+    player.on('playing', function() {
+
+        addClass(s3BubbleMetaOverlayWrap, 's3bubble-meta-overlay-hidden');
+    });
+
+    var hasClass = function(ele, cls) {
+        return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+    }
+
+    var addClass = function(ele, cls) {
+        if (!hasClass(ele, cls)) ele.className += " " + cls;
+    }
+
+    var removeClass = function(ele, cls) {
+        if (hasClass(ele, cls)) {
+            var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+            ele.className = ele.className.replace(reg, ' ');
+        }
+    }
+
+};
+
+/**
+ * A video.js plugin.
+ *
+ * In the plugin function, the value of `this` is a video.js `Player`
+ * instance. You cannot rely on the player being in a "ready" state here,
+ * depending on how the plugin is invoked. This may or may not be important
+ * to you; if not, remove the wait for "ready"!
+ *
+ * @function s3BubbleMetaOverlay
+ * @param    {Object} [options={}]
+ *           An object of options left to the plugin author to define.
+ */
+const s3BubbleMetaOverlay = function(options) {
+    this.ready(() => {
+        onPlayerReady(this, videojs.mergeOptions(defaults, options));
+    });
+};
+
+// Register the plugin with video.js.
+videojs.plugin('s3BubbleMetaOverlay', s3BubbleMetaOverlay);
+
+// Include the version number.
+s3BubbleMetaOverlay.VERSION = '__VERSION__';
+
+export default s3BubbleMetaOverlay;
